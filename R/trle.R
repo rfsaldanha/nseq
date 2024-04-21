454b10962b4c1ce7c0a5fc51dcacad4561d10aa9
Raphael Saldanha <rfsaldanha@gmail.com>
2024-04-21 20:32:26 +0200

    trle function

---
+++ R/trle.R
@@ -0,0 +1,29 @@
+#' Run Length Encoding and return result as a tibble
+#'
+#' Given a tibble object and a variable \code{y}, this function will count the number of occurrence of each element in \code{y} in the sequence that they appear, and return this count as a tibble object.
+#'
+#' @param .data a \code{tibble} object.
+#' @param y character. A variable available on \code{.data}
+#'
+#' @return a \code{tibble} object.
+#'
+#' @seealso [rle()]
+#'
+#' @export
+#'
+#' @examples
+#' trle(c(8,15,20,0,0,0,0,5,9,12))
+trle <- function(.x){
+  # Check assertions
+  checkmate::assert_vector(x = .x)
+
+  # Run length encoding
+  res <- rle(.x)
+
+  # Tibble result
+  tibble::tibble(
+    value = res$values,
+    length = res$lengths,
+  )
+
+}
