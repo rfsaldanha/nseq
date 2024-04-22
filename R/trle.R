bc02fe1e9382f7f01646f3610cea0831306e0230
Raphael Saldanha <rfsaldanha@gmail.com>
2024-04-22 13:38:43 +0200

    .x to x

--- R/trle.R
+++ R/trle.R
@@ -2,7 +2,7 @@
 #'
 #' Given a tibble object and a variable \code{y}, this function will count the number of occurrence of each element in \code{y} in the sequence that they appear, and return this count as a tibble object.
 #'
-#' @param .x a vector.
+#' @param x a vector.
 #'
 #' @return a \code{data.frame} object.
 #'
@@ -12,12 +12,12 @@
 #'
 #' @examples
 #' trle(c(8,15,20,0,0,0,0,5,9,12))
-trle <- function(.x){
+trle <- function(x){
   # Check assertions
-  checkmate::assert_vector(x = .x)
+  checkmate::assert_vector(x = x)
 
   # Run length encoding
-  res <- rle(.x)
+  res <- rle(x)
 
   # Tibble result
   data.frame(
