bb9a21c5c0fe547044c208e233e6f7812f97a0ca
Raphael Saldanha <rfsaldanha@gmail.com>
2024-04-22 13:38:31 +0200

    Documentation of shift function.

--- R/shift.R
+++ R/shift.R
@@ -1,5 +1,23 @@
-# https://stackoverflow.com/questions/26997586/shifting-a-vector
-
+#' Shifts vector values to right or left
+#'
+#' @param x Vector for which to shift values
+#' @param n Number of places to be shifted.
+#'    Positive numbers will shift to the right by default.
+#'    Negative numbers will shift to the left by default.
+#'    The direction can be inverted by the invert parameter.
+#' @param invert Whether or not the default shift directions should be inverted.
+#' @param default The value that should be inserted by default.
+#'
+#' @returns a vector.
+#'
+#' @export
+#'
+#' @examples
+#' # Lag
+#' shift(c(2,3,5,6,7), n = 1, default = 0)
+#' # Lead
+#' shift(c(2,3,5,6,7), n = -1, default = 0)
+#'
 shift <- function(x, n, invert=FALSE, default=NA){
   stopifnot(length(x)>=n)
   if(n==0){
@@ -19,3 +37,5 @@ shift <- function(x, n, invert=FALSE, default=NA){
     return(c(x[seq_len(length(x)-n)+n], rep(default, n)))
   }
 }
+
+# https://stackoverflow.com/questions/26997586/shifting-a-vector
